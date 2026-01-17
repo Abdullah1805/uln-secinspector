bc3c0bd5ac824d4896cc775e0837bd94abc70750
Abdullah1805 <bwd400080@gmail.com>
2026-01-17 21:46:41 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-from engine import SovereignBBEngine
-
-if __name__ == "__main__":
-    target = "https://example.com"
-    engine = SovereignBBEngine(concurrency=40)
-    asyncio.run(engine.run(target))
