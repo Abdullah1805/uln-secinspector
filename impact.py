607aea02fdd4b9e5b924a37802302ba356e47026
Abdullah1805 <bwd400080@gmail.com>
2026-01-17 22:15:50 +0300

    Delete impact.py

--- impact.py
+++
@@ -1,8 +0,0 @@
-class ImpactEngine:
-    def escalate(self, f, evidence):
-        if f["type"] == "PARAM_TEST":
-            return {
-                "impact": "Time-based SQL Injection",
-                "confidence": "High"
-            }
-        return None
