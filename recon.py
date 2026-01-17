5ec29d1e8947c90b8e4a7644ffee619f738e03b2
Abdullah1805 <bwd400080@gmail.com>
2026-01-17 21:56:19 +0300

    Create recon.py

---
+++ recon.py
@@ -0,0 +1,29 @@
+import aiohttp
+import asyncio
+from urllib.parse import urljoin
+
+COMMON_ENDPOINTS = ["/api/user", "/api/profile", "/admin", "/graphql"]
+COMMON_PARAMS = ["id", "user_id", "uid"]
+
+class ReconEngine:
+    def __init__(self, concurrency):
+        self.sem = asyncio.Semaphore(concurrency)
+
+    async def discover(self, base):
+        findings = []
+        async with aiohttp.ClientSession() as session:
+            for ep in COMMON_ENDPOINTS:
+                url = urljoin(base, ep)
+                async with self.sem:
+                    try:
+                        async with session.get(url, timeout=5) as r:
+                            if r.status in (200, 401, 403):
+                                for p in COMMON_PARAMS:
+                                    findings.append({
+                                        "type": "PARAM_TEST",
+                                        "url": url,
+                                        "param": p
+                                    })
+                    except:
+                        pass
+        return findings
